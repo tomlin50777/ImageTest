3ce89770176618e7acde3164a81212a8e1f9d098
tomlin50777 <tomlin50777@hotmail.com>
2021-06-28 07:59:26 +0800

    加入專案檔案。

---
+++ ImageTest/ImageForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace ImageTest
+{
+	partial class ImageForm
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.baseTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.buttonTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.pepperButton = new System.Windows.Forms.Button();
+			this.binaryButton = new System.Windows.Forms.Button();
+			this.locateButton = new System.Windows.Forms.Button();
+			this.blackNoiceClearButton = new System.Windows.Forms.Button();
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.nowLabel = new System.Windows.Forms.Label();
+			this.listBox = new System.Windows.Forms.ListBox();
+			this.baseTableLayoutPanel.SuspendLayout();
+			this.buttonTableLayoutPanel.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// baseTableLayoutPanel
+			// 
+			this.baseTableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.baseTableLayoutPanel.ColumnCount = 2;
+			this.baseTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+			this.baseTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+			this.baseTableLayoutPanel.Controls.Add(this.buttonTableLayoutPanel, 0, 1);
+			this.baseTableLayoutPanel.Controls.Add(this.pictureBox, 0, 0);
+			this.baseTableLayoutPanel.Controls.Add(this.nowLabel, 1, 1);
+			this.baseTableLayoutPanel.Controls.Add(this.listBox, 1, 0);
+			this.baseTableLayoutPanel.Location = new System.Drawing.Point(12, 12);
+			this.baseTableLayoutPanel.Name = "baseTableLayoutPanel";
+			this.baseTableLayoutPanel.RowCount = 2;
+			this.baseTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+			this.baseTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.baseTableLayoutPanel.Size = new System.Drawing.Size(1019, 553);
+			this.baseTableLayoutPanel.TabIndex = 0;
+			// 
+			// buttonTableLayoutPanel
+			// 
+			this.buttonTableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonTableLayoutPanel.ColumnCount = 5;
+			this.buttonTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.buttonTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.buttonTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.buttonTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.buttonTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.buttonTableLayoutPanel.Controls.Add(this.pepperButton, 0, 0);
+			this.buttonTableLayoutPanel.Controls.Add(this.binaryButton, 1, 0);
+			this.buttonTableLayoutPanel.Controls.Add(this.blackNoiceClearButton, 2, 0);
+			this.buttonTableLayoutPanel.Controls.Add(this.locateButton, 3, 0);
+			this.buttonTableLayoutPanel.Location = new System.Drawing.Point(3, 445);
+			this.buttonTableLayoutPanel.Name = "buttonTableLayoutPanel";
+			this.buttonTableLayoutPanel.RowCount = 3;
+			this.buttonTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.buttonTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.buttonTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.buttonTableLayoutPanel.Size = new System.Drawing.Size(707, 105);
+			this.buttonTableLayoutPanel.TabIndex = 0;
+			// 
+			// pepperButton
+			// 
+			this.pepperButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pepperButton.Location = new System.Drawing.Point(3, 3);
+			this.pepperButton.Name = "pepperButton";
+			this.pepperButton.Size = new System.Drawing.Size(135, 29);
+			this.pepperButton.TabIndex = 1;
+			this.pepperButton.Text = "PepperMaker";
+			this.pepperButton.UseVisualStyleBackColor = true;
+			this.pepperButton.Click += new System.EventHandler(this.PepperButton_Click);
+			// 
+			// binaryButton
+			// 
+			this.binaryButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.binaryButton.Location = new System.Drawing.Point(144, 3);
+			this.binaryButton.Name = "binaryButton";
+			this.binaryButton.Size = new System.Drawing.Size(135, 29);
+			this.binaryButton.TabIndex = 2;
+			this.binaryButton.Text = "Binary";
+			this.binaryButton.UseVisualStyleBackColor = true;
+			this.binaryButton.Click += new System.EventHandler(this.BinaryButton_Click);
+			// 
+			// locateButton
+			// 
+			this.locateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.locateButton.Location = new System.Drawing.Point(426, 3);
+			this.locateButton.Name = "locateButton";
+			this.locateButton.Size = new System.Drawing.Size(135, 29);
+			this.locateButton.TabIndex = 3;
+			this.locateButton.Text = "Locate";
+			this.locateButton.UseVisualStyleBackColor = true;
+			this.locateButton.Click += new System.EventHandler(this.LocateButton_Click);
+			// 
+			// blackNoiceClearButton
+			// 
+			this.blackNoiceClearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.blackNoiceClearButton.Location = new System.Drawing.Point(285, 3);
+			this.blackNoiceClearButton.Name = "blackNoiceClearButton";
+			this.blackNoiceClearButton.Size = new System.Drawing.Size(135, 29);
+			this.blackNoiceClearButton.TabIndex = 4;
+			this.blackNoiceClearButton.Text = "Black Noice Clear";
+			this.blackNoiceClearButton.UseVisualStyleBackColor = true;
+			this.blackNoiceClearButton.Click += new System.EventHandler(this.BlackNoiceClearButton_Click);
+			// 
+			// pictureBox
+			// 
+			this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(707, 436);
+			this.pictureBox.TabIndex = 1;
+			this.pictureBox.TabStop = false;
+			// 
+			// nowLabel
+			// 
+			this.nowLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.nowLabel.AutoSize = true;
+			this.nowLabel.Location = new System.Drawing.Point(716, 491);
+			this.nowLabel.Name = "nowLabel";
+			this.nowLabel.Size = new System.Drawing.Size(300, 12);
+			this.nowLabel.TabIndex = 2;
+			this.nowLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// listBox
+			// 
+			this.listBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox.FormattingEnabled = true;
+			this.listBox.ItemHeight = 12;
+			this.listBox.Items.AddRange(new object[] {
+            "共 : 0 S"});
+			this.listBox.Location = new System.Drawing.Point(716, 3);
+			this.listBox.Name = "listBox";
+			this.listBox.Size = new System.Drawing.Size(300, 436);
+			this.listBox.TabIndex = 3;
+			// 
+			// ImageForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1043, 577);
+			this.Controls.Add(this.baseTableLayoutPanel);
+			this.Name = "ImageForm";
+			this.Text = "Image Form";
+			this.baseTableLayoutPanel.ResumeLayout(false);
+			this.baseTableLayoutPanel.PerformLayout();
+			this.buttonTableLayoutPanel.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel baseTableLayoutPanel;
+		private System.Windows.Forms.TableLayoutPanel buttonTableLayoutPanel;
+		private System.Windows.Forms.Button pepperButton;
+		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.Button binaryButton;
+		private System.Windows.Forms.Button locateButton;
+		private System.Windows.Forms.Button blackNoiceClearButton;
+		private System.Windows.Forms.Label nowLabel;
+		private System.Windows.Forms.ListBox listBox;
+	}
+}
+
---
+++ ImageTest/ImageForm.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageTest
+{
+	public partial class ImageForm : System.Windows.Forms.Form
+	{
+		private Bitmap bitmap;
+		private string path;
+		private DateTime now;
+		public ImageForm()
+		{
+			InitializeComponent();
+			path = @"D:\ForYou\Test3.png";
+			bitmap = new Bitmap(path);
+		}
+		private void PepperButton_Click(object sender, EventArgs e)
+		{
+			Start(null);
+			Random random = new Random();
+			Bitmap result = new Bitmap(bitmap.Width, bitmap.Height);
+			int blackPepperNumber = 300;
+			int offset = 15;
+			for (int countY = 0; countY < bitmap.Height; countY++)
+			{
+				for (int countX = 0; countX < bitmap.Width; countX++)
+				{
+					int r = bitmap.GetPixel(countX, countY).R + random.Next(-offset, offset);
+					int g = bitmap.GetPixel(countX, countY).G + random.Next(-offset, offset);
+					int b = bitmap.GetPixel(countX, countY).B + random.Next(-offset, offset);
+					if (r < 0) r = 0;
+					if (r > 255) r = 255;
+					if (g < 0) g = 0;
+					if (g > 255) g = 255;
+					if (b < 0) b = 0;
+					if (b > 255) b = 255;
+					result.SetPixel(countX, countY, Color.FromArgb(r, g, b));
+				}
+			}
+			for (int count = 0; count < blackPepperNumber; count++)
+			{
+				int tempX = random.Next(bitmap.Width);
+				int tempY = random.Next(bitmap.Height);
+				result.SetPixel(tempX, tempY, Color.FromArgb(0, 0, 0));
+			}
+			ImageUpdate(result, "Pepper");
+		}
+		private void BinaryButton_Click(object sender, EventArgs e)
+		{
+			Start("Pepper");
+			int gate = 100;
+			Bitmap result = new Bitmap(bitmap.Width, bitmap.Height);
+			for (int countY = 0; countY < bitmap.Height; countY++)
+			{
+				for (int countX = 0; countX < bitmap.Width; countX++)
+				{
+					Color pixel = bitmap.GetPixel(countX, countY);
+					if(pixel.R + pixel.G + pixel.B > gate)
+						result.SetPixel(countX, countY, Color.FromArgb(255, 255, 255));
+					else
+						result.SetPixel(countX, countY, Color.FromArgb(0, 0, 0));
+				}
+			}
+			ImageUpdate(result, "Binary");
+		}
+		private void BlackNoiceClearButton_Click(object sender, EventArgs e)
+		{
+			Start("Binary");
+			Bitmap result = new Bitmap(bitmap.Width, bitmap.Height);
+			int gate = 3;
+			int countTime = 0;
+			for (int countY = 0; countY < bitmap.Height; countY++)
+				for (int countX = 0; countX < bitmap.Width; countX++)
+					result.SetPixel(countX, countY, bitmap.GetPixel(countX, countY));
+			while (true)
+			{
+				countTime++;
+				bool change = false;
+				for (int countY = 0; countY < result.Height; countY++)
+				{
+					for (int countX = 0; countX < result.Width; countX++)
+					{
+						if (result.GetPixel(countX, countY) == Color.FromArgb(0, 0, 0))
+						{
+							int temp = 0;
+							if (countX == 0 || result.GetPixel(countX - 1, countY) == Color.FromArgb(255, 255, 255))
+								temp++;
+							if (countX == result.Width - 1 || result.GetPixel(countX + 1, countY) == Color.FromArgb(255, 255, 255))
+								temp++;
+							if (countY == 0 || result.GetPixel(countX, countY - 1) == Color.FromArgb(255, 255, 255))
+								temp++;
+							if (countY == result.Height - 1 || result.GetPixel(countX, countY + 1) == Color.FromArgb(255, 255, 255))
+								temp++;
+							if (temp >= gate)
+							{
+								change = true;
+								result.SetPixel(countX, countY, Color.FromArgb(255, 255, 255));
+								countY--;
+								countX = 0;
+							}
+						}
+					}
+				}
+				if (!change)
+					break;
+			}
+			ImageUpdate(result, "Noise Clear");
+		}
+		private void LocateButton_Click(object sender, EventArgs e)
+		{
+			Start("Noise Clear");
+			int[,] map = new int[bitmap.Height, bitmap.Width];
+			int classification = 0;
+			Bitmap result = new Bitmap(bitmap.Width, bitmap.Height);
+			for (int countY = 0; countY < bitmap.Height; countY++)
+				for (int countX = 0; countX < bitmap.Width; countX++)
+					result.SetPixel(countX, countY, bitmap.GetPixel(countX, countY));
+			for (int countY = 0; countY < result.Height; countY++)
+			{
+				for (int countX = 0; countX < result.Width; countX++)
+				{
+					if (result.GetPixel(countX, countY) == Color.FromArgb(255, 255, 255))
+					{
+						LocateImage(result, map, countX, countY, classification);
+						classification++;
+					}
+				}
+			}
+						
+
+		}
+		private void LocateImage(Bitmap result, int[,] map, int x, int y, int classification)
+		{
+
+		}
+		private void Start(string lastFunctionName)
+		{
+			if(lastFunctionName != null)
+				bitmap = new Bitmap(Path.Combine(Path.GetDirectoryName(path), lastFunctionName + "_" + Path.GetFileName(path)));
+			now = DateTime.Now;
+		}
+		private void ImageUpdate(Bitmap result, string functionName)
+		{
+			DateTime end = DateTime.Now;
+			double total = 0;
+			string totalString = (string)listBox.Items[listBox.Items.Count - 1];
+			result.Save(Path.Combine(Path.GetDirectoryName(path), functionName + "_" + Path.GetFileName(path)));
+			pictureBox.Image = new Bitmap(result, pictureBox.Width, pictureBox.Height);
+			pictureBox.Refresh();
+			bitmap = result;
+			nowLabel.Text = functionName + " : " + new TimeSpan(end.Ticks - now.Ticks).TotalSeconds + " S";
+			totalString = totalString.Substring(totalString.IndexOf(": ") + 2, totalString.LastIndexOf(" S") - totalString.IndexOf(": ") - 2);
+			total = Convert.ToDouble(totalString) + new TimeSpan(end.Ticks - now.Ticks).TotalSeconds;
+			listBox.Items[listBox.Items.Count - 1] = nowLabel.Text;
+			listBox.Items.Add("共 : " + total + " S");
+			listBox.Refresh();
+
+			nowLabel.Refresh();
+		}
+	}
+}
